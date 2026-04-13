feff91ec9526d7a6bc1a096bf0f72a90c6ac56b3
WildanFathir <wildanfathir612@gmail.com>
2026-04-13 22:35:48 +0700

    feat: add TypeScript interfaces for movie data models

---
+++ types/movie.ts
@@ -0,0 +1,91 @@
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  release_date: string;
+  vote_average: number;
+  vote_count: number;
+  popularity: number;
+  genres?: Genre[];
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface MovieDetail extends Movie {
+  budget: number;
+  revenue: number;
+  runtime: number;
+  status: string;
+  tagline: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  spoken_languages: Language[];
+  credits: Credits;
+}
+
+export interface ProductionCompany {
+  id: number;
+  name: string;
+  logo_path: string | null;
+  origin_country: string;
+}
+
+export interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface Language {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface Credits {
+  cast: CastMember[];
+  crew: CrewMember[];
+}
+
+export interface CastMember {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string | null;
+  order: number;
+}
+
+export interface CrewMember {
+  id: number;
+  name: string;
+  job: string;
+  department: string;
+  profile_path: string | null;
+}
+
+export interface PaginatedResponse<T> {
+  page: number;
+  results: T[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface SearchResult {
+  id: number;
+  title?: string;
+  name?: string;
+  poster_path: string | null;
+  overview: string;
+  release_date?: string;
+  first_air_date?: string;
+}
+
+export interface UserMovie extends Movie {
+  savedAt: number;
+  rating?: number;
+  notes?: string;
+}
